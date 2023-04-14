c88beebfe33bbb5e987877e395f0dbaf886a68e1
Juan de Dios Martínez Vallejo <vallemardeveloper@gmail.com>
2023-04-14 14:46:37 +0200

    add title animation

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,7 +6,7 @@ import {
   toRaw,
 } from "nativescript-vue";
 import { dataCards } from "@/data"
-import { CoreTypes, isAndroid, PageTransition, SharedTransition, View, ModalTransition, Utils } from "@nativescript/core";
+import { CoreTypes, isAndroid, PageTransition, SharedTransition, View, ModalTransition, Utils, Screen } from "@nativescript/core";
 import { configHomeSharedTransition } from "~/animation";
 import Card from "./Card.vue";
 import Details from "./Details.vue";
@@ -14,6 +14,7 @@ import Details from "./Details.vue";
 const isOpen = ref(false);
 const refShowBtn = ref();
 const refAddBtn = ref();
+const refTextHeader = ref();
 
 const heightCard = 200;
 const transaleY = 70;
@@ -30,46 +31,59 @@ function loadedCard(args: any, index: number) {
 function toggleStatus() {
   const showBtn: View = toRaw(refShowBtn.value.nativeView);
   const addBtn: View = toRaw(refAddBtn.value.nativeView);
+  const textHeader: View = toRaw(refTextHeader.value.nativeView);
 
   if (isOpen.value) {
     viewCards.forEach((cardView, index) => index == 0 || close(cardView, index));
     changeOpacity(showBtn, 0, { rotate: 90 });
     changeOpacity(addBtn, 1, { translate: { y: 0, x: 0 } });
+    if (isAndroid) {
+      animateAndroid(textHeader, { x: 0, y: 0, direction: androidx.dynamicanimation.animation.DynamicAnimation.TRANSLATION_X });
+    } else {
+      animateIOS(textHeader, { x: 0, y: 0 })
+    }
   } else {
     viewCards.forEach((cardView, index) => open(cardView));
     changeOpacity(showBtn, 1, { rotate: 0 });
     changeOpacity(addBtn, 0, { translate: { y: 100, x: 0 } });
+    if (isAndroid) {
+      animateAndroid(textHeader, { x: -(Screen.mainScreen.widthDIPs / 2) + (textHeader.getActualSize().width / 2) + 10, y: 0, direction: androidx.dynamicanimation.animation.DynamicAnimation.TRANSLATION_X });
+    } else {
+      animateIOS(textHeader, { x: -(Screen.mainScreen.widthDIPs / 2) + (textHeader.getActualSize().width / 2) + 10, y: 0 })
+    }
   }
   isOpen.value = !isOpen.value;
 }
 
 function open(cardView: View) {
   if (isAndroid) {
-    animateAndroid(cardView, 0);
+    animateAndroid(cardView, { x: 0, y: 0, direction: androidx.dynamicanimation.animation.DynamicAnimation.TRANSLATION_Y });
   } else {
-    animateIOS(cardView, 0);
+    animateIOS(cardView, { x: 0, y: 0 });
   }
 }
 
 function close(cardView: View, index: number) {
   if (isAndroid) {
-    animateAndroid(cardView, -(heightCard - transaleY) * index);
+    animateAndroid(cardView, { x: 0, y: -(heightCard - transaleY) * index, direction: androidx.dynamicanimation.animation.DynamicAnimation.TRANSLATION_Y });
   } else {
-    animateIOS(cardView, -(heightCard - transaleY) * index);
+    animateIOS(cardView, { x: 0, y: -(heightCard - transaleY) * index });
   }
 }
 
-function animateAndroid(view: View, position: number) {
+function animateAndroid(view: View, position: { x: number, y: number, direction: androidx.dynamicanimation.animation.DynamicAnimation.ViewProperty }) {
   if ((global as any).isAppRunInPreview) {
     view.animate({
-      translate: { x: 0, y: position },
+      translate: { x: position.x, y: position.y },
       curve: CoreTypes.AnimationCurve.cubicBezier(0.1, 0.1, 0.1, 1),
       duration: 250
     });
   } else {
     const springAnim = new androidx.dynamicanimation.animation.SpringAnimation(
-      view.android, androidx.dynamicanimation.animation.DynamicAnimation.TRANSLATION_Y,
-      Utils.layout.toDevicePixels(position)
+      view.android, position.direction,
+      Utils.layout.toDevicePixels(
+        position.direction === androidx.dynamicanimation.animation.DynamicAnimation.TRANSLATION_Y ? position.y : position.x
+      )
     );
     springAnim.getSpring().setStiffness(150);
     springAnim.getSpring().setDampingRatio(0.6);
@@ -77,10 +91,10 @@ function animateAndroid(view: View, position: number) {
   }
 }
 
-function animateIOS(view: View, position: number) {
+function animateIOS(view: View, position: { x: number, y: number }) {
   Utils.ios.animateWithSpring({
     animations: () => {
-      (<UIView>view.ios).transform = CGAffineTransformMakeTranslation(0.0, position)
+      (<UIView>view.ios).transform = CGAffineTransformMakeTranslation(position.x, position.y)
     }
   });
 }
@@ -110,7 +124,6 @@ function openOrGoToDetails(index: number) {
     toggleStatus();
   }
 }
-
 </script>
 
 <template>
@@ -118,11 +131,12 @@ function openOrGoToDetails(index: number) {
     <Page actionBarHidden="true" androidStatusBarBackground="transparent">
       <GridLayout>
         <StackLayout class="h-full" verticalAlignment="top">
-          <GridLayout height="50" class="px-2">
-            <Label text="Wallet" class="text-2xl font-bold text-black" horizontalAlignment="center"></Label>
+          <GridLayout height="50" class="">
+            <Label ref="refTextHeader" text="Wallet" class="text-2xl font-bold text-black"
+              horizontalAlignment="center"></Label>
             <Label ref="refShowBtn" text="close" style="font-size: 24;" height="45" width="45" rotate="90"
-              class="m-icon-round bg-[#0666eb] rounded-full text-white text-center opacity-0" horizontalAlignment="right"
-              @tap="toggleStatus"></Label>
+              class="m-icon-round bg-[#0666eb] rounded-full text-white text-center opacity-0 mr-2"
+              horizontalAlignment="right" @tap="toggleStatus"></Label>
           </GridLayout>
           <FlexboxLayout class="mt-2 h-full flex-col">
             <Card v-for="(card, index) in dataCards" :key="index" :style="{ 'height': heightCard }" :data="card"
